9549d982adbed8cd438324f7c9f3a3268207cca7
m.vorobyev <max9680@mail.ru>
2022-11-11 16:24:54 +0300

    Add maintainability

--- src/differ.php
+++ src/differ.php
@@ -25,33 +25,29 @@ function genDiff($pathFile1, $pathFile2)
     ksort($array2);
 
     foreach ($array1 as $key => $value) {
-        
         if (array_key_exists($key, $array2)) {
-            
             if ($array1[$key] === $array2[$key]) {
-
-                $result .= (is_bool($array1[$key])) 
+                $result .= (is_bool($array1[$key]))
                 ? "  " . "$key" . ": " . boolValueAsString($key, $array1[$key])
                  . "\n"
                 : "  " . "$key" . ": " . "$array1[$key]\n";
                 
                 unset($array2[$key]);
             } else {
-
-                $result .= (is_bool($array1[$key])) 
+                $result .= (is_bool($array1[$key]))
                 ? "- " . "$key" . ": " . boolValueAsString($key, $array1[$key])
-                 . "\n" 
+                 . "\n"
                 : "- " . "$key" . ": " . "$array1[$key]\n";
 
-                $result .= (is_bool($array2[$key])) 
+                $result .= (is_bool($array2[$key]))
                 ? "+ " . "$key" . ": " . boolValueAsString($key, $array2[$key])
-                 . "\n" 
+                 . "\n"
                 : "+ " . "$key" . ": " . "$array2[$key]\n";
 
                 unset($array2[$key]);
             }
         } else {
-            $result .= (is_bool($array1[$key])) 
+            $result .= (is_bool($array1[$key]))
             ? "- " . "$key" . ": " . boolValueAsString($key, $array1[$key])
              . "\n"
             : "- " . "$key" . ": " . "$array1[$key]\n";
@@ -60,9 +56,9 @@ function genDiff($pathFile1, $pathFile2)
 
 
     foreach ($array2 as $key => $value) {
-        $result .= (is_bool($array2[$key])) 
-        ? "+ " . "$key" . ": " . boolValueAsString($key, $array2[$key]) 
-        . "\n" 
+        $result .= (is_bool($array2[$key]))
+        ? "+ " . "$key" . ": " . boolValueAsString($key, $array2[$key])
+        . "\n"
         : "+ " . "$key" . ": " . "$array2[$key]\n";
     }
 
