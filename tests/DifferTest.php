b88bb50ca58c813afc6a7320bb06b0ba09d670db
m.vorobyev <max9680@mail.ru>
2022-11-14 20:50:51 +0300

    Create differ-check.yml

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -7,13 +7,7 @@ class DifferTest extends TestCase
 {
     public function testGenDiff(): void
     {
-        $testResult = "
-        - follow: false
-          host: hexlet.io
-        - proxy: 123.234.53.22
-        - timeout: 50
-        + timeout: 20
-        + verbose: true";
+        $testResult = "- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n";
         $this->assertEquals($testResult, genDiff("/home/maxim/php-project-48/file1.json", "/home/maxim/php-project-48/file2.json"));
     }
 }
