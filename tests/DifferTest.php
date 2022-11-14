b58fbb70e0b3c33525539536143b8b47b164dd34
m.vorobyev <max9680@mail.ru>
2022-11-14 22:11:47 +0300

    Add fixtures

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -8,6 +8,6 @@ class DifferTest extends TestCase
     public function testGenDiff(): void
     {
         $testResult = "- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n";
-        $this->assertEquals($testResult, genDiff("/home/maxim/php-project-48/file1.json", "/home/maxim/php-project-48/file2.json"));
+        $this->assertEquals($testResult, genDiff("./tests/fixtures/file1.json", "./tests/fixtures/file2.json"));
     }
 }
