4acc819618131d3fed70de2442fdc33fc32a0824
m.vorobyev <max9680@mail.ru>
2022-12-03 15:26:23 +0300

    Modify Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -4,13 +4,138 @@ namespace Differ\Differ;
 
 use function Differ\Parsers\getContentFromFile;
 
-function boolValueAsString($key, $value)
+function valueAsString($value)
 {
-    if ($value === true) {
-        return "true";
-    } else {
-        return "false";
+    if (is_bool($value)) {
+        if ($value === true) {
+            return "true";
+        } else {
+            return "false";
+        }
     }
+
+    if ($value === null) {
+        return "null";
+    }
+
+    return $value;
+}
+
+function createIntendant(int $depth, int $mode): string
+{
+    $intendants = ["    ", '  - ', '  + '];
+
+    switch ($mode) {
+    case 0:
+        return str_repeat($intendants[0], $depth) . $intendants[0];
+        break;
+    case 1:
+        return str_repeat($intendants[0], $depth) . $intendants[1];
+    case 2:
+        return str_repeat($intendants[0], $depth) . $intendants[2];
+    }
+}
+
+function getArrayValues(array $array, int $depth, string $outputString = ""): string
+{
+    foreach ($array as $key => $value) {
+        if (is_array($value)) {
+            $outputString = $outputString . createIntendant($depth, 0) . $key .
+            ": {\n" . getArrayValues($value, $depth + 1) . "\n";
+        } else {
+            $outputString = $outputString . createIntendant($depth, 0) . $key .
+            ": " . valueAsString($value) . "\n";
+        }
+    }
+
+    $outputString = $outputString . createIntendant($depth - 1, 0) . "}";
+
+    return $outputString;
+}
+
+function iter($array1, $array2, $depth, $result = "")
+{
+
+    $result = $result . "{\n";
+
+    $mergeKeys = array_unique(array_merge(array_keys($array1), array_keys($array2)));
+    asort($mergeKeys);
+
+    foreach ($mergeKeys as $mergeKey) {
+        // Key exists in array1 and array2
+        if (array_key_exists($mergeKey, $array1)
+            && array_key_exists($mergeKey, $array2)
+        ) {
+            //Value is array in $array1 and $array2
+            if (is_array($array1[$mergeKey]) && is_array($array2[$mergeKey])) {
+                $result = $result . createIntendant($depth, 0) . $mergeKey .
+                ": " . iter($array1[$mergeKey], $array2[$mergeKey], $depth + 1);
+            }
+
+            //Value is NOT array in $array1 and NOT array in $array2
+            if (!is_array($array1[$mergeKey]) && !is_array($array2[$mergeKey])) {
+                if ($array1[$mergeKey] == $array2[$mergeKey]) {
+                    $result = $result . createIntendant($depth, 0) . $mergeKey .
+                    ": " . valueAsString($array1[$mergeKey]) . "\n";
+                } else {
+                    $result = $result . createIntendant($depth, 1) . $mergeKey .
+                    ": " . valueAsString($array1[$mergeKey]) . "\n";
+                    $result = $result . createIntendant($depth, 2) . $mergeKey .
+                    ": " . valueAsString($array2[$mergeKey]) . "\n";
+                }
+            }
+
+            //Value is array in $array1 and NOT array in $array2
+            if (is_array($array1[$mergeKey]) && !is_array($array2[$mergeKey])) {
+                $result = $result . createIntendant($depth, 1) . $mergeKey .
+                ": {\n" . getArrayValues($array1[$mergeKey], $depth + 1) . "\n";
+                $result = $result . createIntendant($depth, 2) . $mergeKey .
+                ": " . valueAsString($array2[$mergeKey]) . "\n";
+            }
+
+            //Value is NOT array in $array1 and array in $array2
+            if (!is_array($array1[$mergeKey]) && is_array($array2[$mergeKey])) {
+                $result = $result . createIntendant($depth, 1) . $mergeKey .
+                ": " . valueAsString($array1[$mergeKey]) . "\n";
+                $result = $result . createIntendant($depth, 2) . $mergeKey .
+                ": {\n" . getArrayValues($array2[$mergeKey], $depth + 1) . "\n";
+            }
+        }
+
+        //Key exists in array1 and doesn't exist in array2
+        if (array_key_exists($mergeKey, $array1)
+            && !(array_key_exists($mergeKey, $array2))
+        ) {
+            if (is_array($array1[$mergeKey])) {
+                $result = $result . createIntendant($depth, 1) . $mergeKey .
+                ": {\n" . getArrayValues($array1[$mergeKey], $depth + 1) . "\n";
+            } else {
+                $result = $result . createIntendant($depth, 1) . $mergeKey .
+                ": " . valueAsString($array1[$mergeKey]) . "\n";
+            }
+        }
+
+        //Key doesn't exist in array1 and exists in array2
+        if (!(array_key_exists($mergeKey, $array1))
+            && array_key_exists($mergeKey, $array2)
+        ) {
+            if (is_array($array2[$mergeKey])) {
+                $result = $result . createIntendant($depth, 2) . $mergeKey .
+                ": {\n" . getArrayValues($array2[$mergeKey], $depth + 1) . "\n";
+            } else {
+                $result = $result . createIntendant($depth, 2) . $mergeKey . ":
+                " . valueAsString($array2[$mergeKey]) . "\n";
+            }
+        }
+    }
+
+    if ($depth == 0) {
+        $result = $result . "}";
+    } else {
+        $result = $result . createIntendant($depth - 1, 0) . "}\n";
+    }
+
+    return $result;
 }
 
 function genDiff($pathFile1, $pathFile2)
@@ -18,48 +143,5 @@ function genDiff($pathFile1, $pathFile2)
     $array1 = getContentFromFile($pathFile1);
     $array2 = getContentFromFile($pathFile2);
 
-    $result = "";
-
-    ksort($array1);
-    ksort($array2);
-
-    foreach ($array1 as $key => $value) {
-        if (array_key_exists($key, $array2)) {
-            if ($array1[$key] === $array2[$key]) {
-                $result .= (is_bool($array1[$key]))
-                ? "  " . "$key" . ": " . boolValueAsString($key, $array1[$key])
-                 . "\n"
-                : "  " . "$key" . ": " . "$array1[$key]\n";
-
-                unset($array2[$key]);
-            } else {
-                $result .= (is_bool($array1[$key]))
-                ? "- " . "$key" . ": " . boolValueAsString($key, $array1[$key])
-                 . "\n"
-                : "- " . "$key" . ": " . "$array1[$key]\n";
-
-                $result .= (is_bool($array2[$key]))
-                ? "+ " . "$key" . ": " . boolValueAsString($key, $array2[$key])
-                 . "\n"
-                : "+ " . "$key" . ": " . "$array2[$key]\n";
-
-                unset($array2[$key]);
-            }
-        } else {
-            $result .= (is_bool($array1[$key]))
-            ? "- " . "$key" . ": " . boolValueAsString($key, $array1[$key])
-             . "\n"
-            : "- " . "$key" . ": " . "$array1[$key]\n";
-        }
-    }
-
-
-    foreach ($array2 as $key => $value) {
-        $result .= (is_bool($array2[$key]))
-        ? "+ " . "$key" . ": " . boolValueAsString($key, $array2[$key])
-        . "\n"
-        : "+ " . "$key" . ": " . "$array2[$key]\n";
-    }
-
-    return $result;
+    return iter($array1, $array2, 0);
 }
