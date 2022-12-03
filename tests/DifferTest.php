89457b7bc0ed53928697f87a3523559e030e3f83
m.vorobyev <max9680@mail.ru>
2022-12-03 16:15:26 +0300

    modified:   tests/DifferTest.php

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -15,49 +15,49 @@ class DifferTest extends TestCase
     public function testGenDiffNested(): void
     {
         $testResult = "{
-            common: {
-              + follow: false
-                setting1: Value 1
-              - setting2: 200
-              - setting3: true
-              + setting3: null
-              + setting4: blah blah
-              + setting5: {
-                    key5: value5
-                }
-                setting6: {
-                    doge: {
-                      - wow:
-                      + wow: so much
-                    }
-                    key: value
-                  + ops: vops
-                }
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow:
+              + wow: so much
             }
-            group1: {
-              - baz: bas
-              + baz: bars
-                foo: bar
-              - nest: {
-                    key: value
-                }
-              + nest: str
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
             }
-          - group2: {
-                abc: 12345
-                deep: {
-                    id: 45
-                }
-            }
-          + group3: {
-                deep: {
-                    id: {
-                        number: 45
-                    }
-                }
-                fee: 100500
-            }
-        }";
+        }
+        fee: 100500
+    }
+}";
         $this->assertEquals($testResult, genDiff("./tests/fixtures/file1n.json", "./tests/fixtures/file2n.json"));
     }
 }
