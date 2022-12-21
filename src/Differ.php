809ec4f976019bb6df09b563951a73c679b770a4
m.vorobyev <max9680@mail.ru>
2022-12-21 11:16:58 +0300

    Refactoring

--- src/Differ.php
+++ src/Differ.php
@@ -25,7 +25,8 @@ function diff(array $array1, array $array2): array
         ) {
           //Value is array in $array1 and $array2
             if (is_array($array1[$mergeKey]) && is_array($array2[$mergeKey])) {
-                $result[EQUAL . $mergeKey] = diff($array1[$mergeKey], $array2[$mergeKey]);
+                $arrayDiff = diff($array1[$mergeKey], $array2[$mergeKey]);
+                $result[EQUAL . $mergeKey] = $arrayDiff;
                 return $result;
             }
 
