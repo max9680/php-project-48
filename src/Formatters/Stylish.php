45d35708958f081faa5711d599a4aab479a33372
m.vorobyev <max9680@mail.ru>
2023-01-03 23:34:58 +0300

    modified:   src/Formatters/Stylish.php

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -7,9 +7,6 @@ use function Differ\Formatters\valueAsString;
 function stylish(array $array, int $depth = 1): string
 {
         $output = array_reduce($array, function (string $result, array $item) use ($depth) {
-            if (!is_array($item)) {
-                print_r($item);
-            }
 
             switch ($item['action']) {
                 case 'same':
@@ -20,6 +17,7 @@ function stylish(array $array, int $depth = 1): string
                         return $result . str_repeat('    ', $depth) . $item['property'] . ": "
                         . valueAsString($item['value']) . "\n";
                     }
+
                 case 'added':
                     if (is_array($item['value'])) {
                         return $result . str_repeat('    ', $depth - 1) . '  + ' . $item['property']
@@ -28,6 +26,7 @@ function stylish(array $array, int $depth = 1): string
                         return $result . str_repeat('    ', $depth - 1) . '  + ' . $item['property']
                         . ": " . valueAsString($item['value']) . "\n";
                     }
+
                 case 'removed':
                     if (is_array($item['value'])) {
                         return $result . str_repeat('    ', $depth - 1) . '  - ' . $item['property']
@@ -37,6 +36,7 @@ function stylish(array $array, int $depth = 1): string
                         return $result . str_repeat('    ', $depth - 1) . '  - ' . $item['property'] . ": "
                         . valueAsString($item['value']) . "\n";
                     }
+
                 case 'updated':
                     if (is_array($item['value'])) {
                         if (is_array($item['new value'])) {
