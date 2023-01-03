a4416183b951268cca6d6d754fbef94336dc8b5c
m.vorobyev <max9680@mail.ru>
2023-01-03 23:12:33 +0300

    Modify plain

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -21,21 +21,21 @@ function plain(array $array, string $path = ''): string
 {
     $output = array_reduce($array, function ($result, $item) use ($path) {
         if ($item['action'] == 'same' && is_array($item['value'])) {
-            $result = $result . plain($item['value'], $path . $item['property'] . ".");
+            return $result . plain($item['value'], $path . $item['property'] . ".");
         }
 
         if ($item['action'] == 'added') {
-            $result = $result . "Property " . normalizeValue($path . $item['property'])
+            return $result . "Property " . normalizeValue($path . $item['property'])
             . " was added with value: " . normalizeValue($item['value']) . "\n";
         }
 
         if ($item['action'] == 'removed') {
-            $result = $result . "Property " . normalizeValue($path . $item['property'])
+            return $result . "Property " . normalizeValue($path . $item['property'])
             . " was removed\n";
         }
 
         if ($item['action'] == 'updated') {
-            $result = $result . "Property " . normalizeValue($path . $item['property']) . " was updated. From "
+            return $result . "Property " . normalizeValue($path . $item['property']) . " was updated. From "
             . normalizeValue($item['value']) . " to " . normalizeValue($item['new value']) . "\n";
         }
 
