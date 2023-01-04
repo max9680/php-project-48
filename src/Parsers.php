e988763532c916e276f1801aeed64ae4168d99d7
m.vorobyev <max9680@mail.ru>
2023-01-04 14:12:18 +0300

    Modify parsers.php

--- src/Parsers.php
+++ src/Parsers.php
@@ -6,13 +6,17 @@ use Symfony\Component\Yaml\Yaml;
 
 function getContentFromFile(string $pathFile): array
 {
+    if (!file_exists($pathFile)) {
+        throw new \Exception("Input file not found");
+    }
+
     $explodeFilePath = explode(".", $pathFile);
     $formatFile = end($explodeFilePath);
 
     switch ($formatFile) {
         case 'json':
-                $contentFile = file_get_contents($pathFile);
-                $resultArray = json_decode($contentFile, true, 512, JSON_OBJECT_AS_ARRAY);
+            $contentFile = file_get_contents($pathFile);
+            $resultArray = json_decode($contentFile, true, 512, JSON_OBJECT_AS_ARRAY);
             return $resultArray;
 
         case 'yml':
