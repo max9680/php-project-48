9ded9d67ee5ed7bfff105dc50d70cd99a6b3cd17
m.vorobyev <max9680@mail.ru>
2023-01-12 12:28:23 +0300

    modified:   tests/DifferTest.php

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -8,13 +8,6 @@ use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
-    public function testGenDiff(): void
-    {
-        $testResult = "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}";
-        $this->assertEquals($testResult, genDiff("./tests/fixtures/file1.json", "./tests/fixtures/file2.json"));
-        $this->assertEquals($testResult, genDiff("./tests/fixtures/file1.yml", "./tests/fixtures/file2.yml"));
-    }
-
     public function testGenDiffNested(): void
     {
         $testResult = file_get_contents("tests/fixtures/stylish.txt");
